7c9616f41b7b5d52c4355570fc84ba12bea01250
victorlisboa <victorhugof.lisboa@gmail.com>
2022-11-19 00:29:05 -0300

    Adicionando LIS e template

---
+++ C++/Template/template.cpp
@@ -0,0 +1,8 @@
+#define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define input(x) for (auto &it : x) cin >> it;
+#define output(x) for (auto &it : x) cout << it << ' ';
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define dbg(msg, x) cout << msg << " " << x << endl;
+const double PI = acos(-1);
+const int INF = 0x3f3f3f3f;
+const long long LLINF = 0x3f3f3f3f3f3f3f3f;
