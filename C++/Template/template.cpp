ecc084ae54e28f66317743494cb6bb2b6202c16a
victorlisboa <victorhugof.lisboa@gmail.com>
2022-11-19 01:04:55 -0300

    Últimos ajustes

--- C++/Template/template.cpp
+++ C++/Template/template.cpp
@@ -4,7 +4,6 @@ cout.tie(NULL);
 #define input(x) for (auto &it : x) cin >> it;
 #define output(x) for (auto &it : x) cout << it << ' ';
 #define rep(i, a, b) for (int i = a; i < b; i++)
-#define dbg(msg, x) cout << msg << " " << x << endl;
 const double PI = acos(-1);
 const int INF = 0x3f3f3f3f;
 const long long LLINF = 0x3f3f3f3f3f3f3f3f;
